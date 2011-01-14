4a5d76d245b8e2714de9bb8ad2c372f0e7a80f56
Rafa de Castro <rafael@micubiculo.com>
2011-01-14 17:56:48 +0100

    Adding deprecation messages to compilation

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 rm -rf classes
 mkdir classes
-scalac -cp specs_2.8.1-1.6.7.jar -d classes fizzbuzz.scala
+scalac -deprecation -cp specs_2.8.1-1.6.7.jar -d classes fizzbuzz.scala
 scala -cp specs_2.8.1-1.6.7.jar:classes run fizzBuzzSpec
