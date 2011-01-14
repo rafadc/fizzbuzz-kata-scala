0fdbd40e6b1eb6976ee0359f6141df683a2fd430
Rafa de Castro <rafael@micubiculo.com>
2011-01-14 17:44:46 +0100

    Initial commit

---
+++ fizzbuzz.scala
@@ -0,0 +1,14 @@
+import org.specs.Specification
+
+
+
+class fizzBuzzSpec extends Specification {
+
+  "fizzBuzz function" should {
+    "return itself if parameter non divisible by neither 3 nor 5" in {
+        
+    }
+
+  }
+}
+
