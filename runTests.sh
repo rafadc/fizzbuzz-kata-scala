5e7a83fe0f120b4835380f959d80034eba8324bd
Rafa de Castro <rafael@micubiculo.com>
2011-01-18 00:00:31 +0100

    Running also tests from printer

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,6 @@
 rm -rf classes
 mkdir classes
 scalac -deprecation -cp specs_2.8.1-1.6.7.jar:mockito-all-1.8.5.jar -d classes fizzbuzz.scala fizzprinter.scala
-scala -cp specs_2.8.1-1.6.7.jar:mockito-1.8.5.jar classes run fizzBuzzSpec
+scala -cp specs_2.8.1-1.6.7.jar:mockito-all-1.8.5.jar:classes run fizzBuzzSpec
+scala -cp specs_2.8.1-1.6.7.jar:mockito-all-1.8.5.jar:classes run fizzPrinterSpec
+
