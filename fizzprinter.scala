e96bb558d3ab8c8f557158a8acb61ded1ba12a83
Rafa de Castro <rafael@micubiculo.com>
2011-01-18 01:14:33 +0100

    Refactor to allow printing method to be mocked

--- fizzprinter.scala
+++ fizzprinter.scala
@@ -1,10 +1,21 @@
 import org.specs.Specification
 import org.specs.mock.Mockito
+import org.mockito.Matchers._
 
-class FizzPrinter(calculator : FizzCalculator){
+trait Printer {
+  def print(value : String)
+}
+
+class SimplePrinter extends Printer {
+  def print(value : String) {
+    println(value)
+  } 
+}
+
+class FizzPrinter(calculator : FizzCalculator, printer : Printer){
 
   def printFrom1To100 {
-    (1 until 100 inclusive) foreach { value => println(calculator.calculate(value)) }
+    (1 until 100 inclusive) foreach { value => printer.print(calculator.calculate(value)) }
   }
 }
 
@@ -12,12 +23,17 @@ class fizzPrinterSpec extends Specification with Mockito {
   "Fizzprinter" should {
     "print fizzbuzz for numbers for 1 to 100" in {
        val fizzCalculator = mock[FizzCalculator]
+       val printer = mock[Printer]
 
-       val fizzPrinter = new FizzPrinter(fizzCalculator)
+       val fizzPrinter = new FizzPrinter(fizzCalculator,printer)
+
+       (1 until 100 inclusive) foreach {value => fizzCalculator.calculate(anyInt()) returns value.toString }
 
        fizzPrinter.printFrom1To100
 
-       (1 until 100 inclusive) foreach { value => there was one(fizzCalculator).calculate(value) } 
+       (1 until 100 inclusive) foreach { value => there was one(fizzCalculator).calculate(value) }
+       (1 until 100 inclusive) foreach { value => there was one(printer).print(value.toString) }
+       
     }
   }
 }
